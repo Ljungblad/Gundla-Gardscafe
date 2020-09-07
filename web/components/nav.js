e6f9137b8d55ce9546e40746947a2c80e3ad5630
Henric <henric.bjorkvall@gmail.com>
2020-09-07 19:01:21 +0200

    added state to open and close sidebar menu

--- web/components/nav.js
+++ web/components/nav.js
@@ -1,30 +1,29 @@
-import styled from "styled-components";
-import SideBar from "../components/sidebar";
+import styled from 'styled-components';
+import SideBar from '../components/sidebar';
 
 const StyledNav = styled.nav`
-  height: 50px;
+  height: 61px;
   background: grey;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0 20px 0 20px;
   position: relative;
+
+  a {
+    color: red;
+    z-index: 1;
+  }
 `;
 
 const Nav = () => {
-  const [isHidden, setIsHidden] = React.useState("");
-
-  const toggleHideMenu = () => {
-    const updatedMenu = isHidden === "hidden" ? "" : "hidden";
-    setIsHidden(updatedMenu);
-  };
+  const [open, setOpen] = React.useState(false);
 
   return (
     <StyledNav>
       <a>Logo</a>
-      <a>Map</a>
-      <a onClick={toggleHideMenu}>Menu</a>
-      <SideBar hidden={isHidden}></SideBar>
+      <a onClick={() => setOpen(!open)}>Menu</a>
+      <SideBar open={open}></SideBar>
     </StyledNav>
   );
 };
