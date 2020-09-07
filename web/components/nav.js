4e0239c93c51ab62f912ee81f914b3fc8647867b
Victor Ljungblad <victor.ljungblad@gmail.com>
2020-09-07 14:38:32 +0200

    added sidebar component

--- web/components/nav.js
+++ web/components/nav.js
@@ -1,10 +1,32 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import SideBar from "../components/sidebar";
 
 const StyledNav = styled.nav`
   height: 50px;
   background: grey;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px 0 20px;
+  position: relative;
 `;
 
-const Nav = () => <StyledNav></StyledNav>;
+const Nav = () => {
+  const [isHidden, setIsHidden] = React.useState("");
+
+  const toggleHideMenu = () => {
+    const updatedMenu = isHidden === "hidden" ? "" : "hidden";
+    setIsHidden(updatedMenu);
+  };
+
+  return (
+    <StyledNav>
+      <a>Logo</a>
+      <a>Map</a>
+      <a onClick={toggleHideMenu}>Menu</a>
+      <SideBar hidden={isHidden}></SideBar>
+    </StyledNav>
+  );
+};
 
 export default Nav;
