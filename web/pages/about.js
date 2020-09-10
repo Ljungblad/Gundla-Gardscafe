93e38154f4c313d37143723c47c8e25e9c6aff16
Henric <henric.bjorkvall@gmail.com>
2020-09-10 16:09:00 +0200

    installed npm package swr

--- web/pages/about.js
+++ web/pages/about.js
@@ -2,31 +2,19 @@ import Layout from '../components/layout';
 import groq from 'groq';
 import client from '../client';
 
-const About = (props, propz) => {
-    console.log(propz);
-  return <Layout props={props}></Layout>;
+const About = (props) => {
+  return <Layout navigationLinks={props.navigationLinks.navigation}></Layout>;
 };
+const query = groq`{
+    "navigation": (*[_type == 'navigation']),
+  }`;
 
-// const query = groq`{
-//     "navigation": (*[_type == 'navigation']),
-//   }`;
+export async function getStaticProps(context) {
+  const res = await client.fetch(query);
 
-// About.getInitialProps = async function () {
-//   const res = await client.fetch(query);
-
-//   const resInsta = await fetch(
-//     'https://www.instagram.com/gundlacafeinsta/?__a=1'
-//   );
-//   const InstagramJson = await resInsta.json();
-
-//   return {
-//     props: {
-//       instagram: InstagramJson,
-//       navigation: res,
-//     },
-//   };
-// };
-
-
+  return {
+    props: { navigationLinks: res },
+  };
+}
 
 export default About;
--- web/pages/index.js
+++ web/pages/index.js
@@ -2,6 +2,7 @@ import groq from 'groq';
 import client from '../client';
 import Layout from '../components/layout';
 import InstagramFeed from '../components/instagramfeed';
+import useSWR from 'swr';
 
 const Index = (props) => {
   return (
@@ -10,6 +11,16 @@ const Index = (props) => {
     </Layout>
   );
 };
+
+// const Index = (props) => {
+//   console.log(props);
+//   const { data, error } = useSWR('https://api.kanye.rest', fetcher);
+//   console.log(data);
+
+//   return <h1>hi</h1>;
+// };
+
+// const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const query = groq`{
     "navigation": (*[_type == 'navigation']),
