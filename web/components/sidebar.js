fd783414362438260df93f821fabd3e3c4d84be2
Henric <henric.bjorkvall@gmail.com>
2020-09-11 10:31:33 +0200

    added basic styling to sideBar component

--- web/components/sidebar.js
+++ web/components/sidebar.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
-import Link from 'next/link'
+import Link from 'next/link';
 
 const StyledSideBar = styled.div`
   height: 100vh;
-  width: ${({ open }) => (open ? '100px' : '0px')};
+  width: ${({ open }) => (open ? '250px' : '0px')};
   background: hotpink;
   position: absolute;
   display: flex;
@@ -12,6 +12,26 @@ const StyledSideBar = styled.div`
   right: 0;
   overflow: hidden;
   transition: 0.4s;
+  padding: ${({ open }) => (open ? '0px 30px 0px 30px' : '0px')};
+
+  .sideBarLogo {
+    margin-top: 85px;
+  }
+
+  .navigationLinks {
+    display: flex;
+    flex-direction: column;
+    white-space: nowrap;
+    margin-top: 65px;
+
+    a {
+      height: 60px;
+      display: flex;
+      align-items: center;
+      text-decoration: none;
+      color: #000;
+    }
+  }
 `;
 
 const SideBar = (props) => {
@@ -19,11 +39,14 @@ const SideBar = (props) => {
 
   return (
     <StyledSideBar open={props.open}>
-      {navigationLinks.map((item, i) => (
-        <a href='#' key={i}>
-          {item.title}
-        </a>
-      ))}
+      <h1 className="sideBarLogo">LOGO</h1>
+      <div className='navigationLinks'>
+        {navigationLinks.map((item, i) => (
+          <a href='#' key={i}>
+            {item.title}
+          </a>
+        ))}
+      </div>
     </StyledSideBar>
   );
 };
