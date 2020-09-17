4b929aadc6b8079d7c1cf0ffbdf0398ac0748d6d
Victor Ljungblad <victor.ljungblad@gmail.com>
2020-09-17 16:08:50 +0200

    added links to fastmenu

--- web/components/menubtn.js
+++ web/components/menubtn.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const StyledBtn = styled.button`
   width: 100px;
@@ -6,20 +6,26 @@ const StyledBtn = styled.button`
   border: none;
   background: none;
 
+  a {
+    text-decoration: none;
+  }
+
   p {
     height: 30px;
     display: flex;
     justify-content: center;
     align-items: center;
-    background: ${({ inverted }) => (inverted ? '#000' : '#FFF')};
-    color: ${({ inverted }) => (inverted ? '#FFF' : '#000')};
+    background: ${({ inverted }) => (inverted ? "#000" : "#FFF")};
+    color: ${({ inverted }) => (inverted ? "#FFF" : "#000")};
   }
 `;
 
 const MenuBtn = (props) => {
   return (
     <StyledBtn inverted={props.isActive} onClick={props.onClick}>
-      <p>{props.text}</p>
+      <a href={props.link}>
+        <p>{props.text}</p>
+      </a>
     </StyledBtn>
   );
 };
